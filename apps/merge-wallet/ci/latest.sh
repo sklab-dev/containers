2011a9bece0f07045c2de73b3bfcc82453c0ddcc
aviator-coding <Sascha.s.krumbach@gmail.com>
2025-02-16 13:11:48 -0500

    fix(actions): Merge

--- apps/merge-wallet/ci/latest.sh
+++ apps/merge-wallet/ci/latest.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/home-assistant/core/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
+# version=$(curl -sX GET "https://api.github.com/repos/projectmerge/merge/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
+# version="${version#*v}"
+# version="${version#*release-}"
+version="1.0.3"
 printf "%s" "${version}"
