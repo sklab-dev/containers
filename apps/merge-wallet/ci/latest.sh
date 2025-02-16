50d3db1221e8c89bad4137c7a46da033f5a9cc2a
aviator-coding <Sascha.s.krumbach@gmail.com>
2025-02-16 16:04:03 -0500

    fix(actions)

--- apps/merge-wallet/ci/latest.sh
+++ apps/merge-wallet/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # version=$(curl -sX GET "https://api.github.com/repos/projectmerge/merge/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-# version="${version#*v}"
-# version="${version#*release-}"
 version="1.0.3"
+version="${version#*v}"
+version="${version#*release-}"
 printf "%s" "${version}"
